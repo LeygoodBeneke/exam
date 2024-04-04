6839874d1eb66ab122b0ca238f32da1a8ae77263
VasiliyDenisov <mr.v.m.denisov@mail.ru>
2024-04-04 14:11:07 +0300

    Initial clicker

---
+++ src/main/java/com/example/clicker/ClickerApplication.java
@@ -0,0 +1,24 @@
+package com.example.clicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ClickerApplication {
+	int counter = 0;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickerApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		counter++;
+		return String.format("Hello %s! Counter: %d", name, counter);
+	}
+
+}
